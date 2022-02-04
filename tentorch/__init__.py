e48fbf9367021ef1ffe284e335408c535c19bb2f
joserapa <98589786+joserapa@users.noreply.github.com>
2022-02-04 20:28:31 +0100

    init

--- tentorch/__init__.py
+++ tentorch/__init__.py
@@ -2,5 +2,8 @@ from tentorch.network_components import (AbstractNode, Node, CopyNode, StackNode
                                          ParamNode)
 from tentorch.network_components import (AbstractEdge, Edge, StackEdge,
                                          ParamEdge)
+from tentorch.network_components import TensorNetwork
 
-from tentorch.network import TensorNetwork
+from tentorch.network_components import (contract,
+                                         contract_between,
+                                         batched_contract_between)

865c4e003eb6d31562268f9907ad4f598537db1a
joserapa98 <98589786+joserapa@users.noreply.github.com>
2022-02-21 13:49:36 +0100

    Correct TODO's

--- tentorch/network_operations.py
+++ tentorch/network_operations.py
@@ -1,7 +1,6 @@
 # contraction paths, einsum definition of networks, compatibility with tensornetwork
 
 # TODO: functions to transform between tensornetwork, tntorch and tentorch
-# TODO: functions to find contraction paths using tensornetwork
-
+# TODO: functions to find contraction paths using opt_einsum (like tensornetwork)
 # TODO: implement einsum for Nodes, so that edge links are automatically
-#  made and we can compute contractions between several nodes at the same time
+#  made and we can compute contractions between several nodes at the same time -> schematic way of constructing TN

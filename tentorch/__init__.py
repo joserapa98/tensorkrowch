e56b768f843d38fc1c9c906fa6cf09f212752c0b
joserapa98 <98589786+joserapa@users.noreply.github.com>
2022-02-22 12:16:14 +0100

    Correct imports

--- tentorch/__init__.py
+++ tentorch/__init__.py
@@ -1,9 +1,5 @@
-from tentorch.network_components import (AbstractNode, Node, CopyNode, StackNode,
-                                         ParamNode)
-from tentorch.network_components import (AbstractEdge, Edge, StackEdge,
-                                         ParamEdge)
+from tentorch.network_components import AbstractNode, Node, CopyNode, ParamNode
+from tentorch.network_components import AbstractEdge, Edge, ParamEdge
 from tentorch.network_components import TensorNetwork
 
-from tentorch.network_components import (contract,
-                                         contract_between,
-                                         batched_contract_between)
+from tentorch.network_components import connect, disconnect, contract_between

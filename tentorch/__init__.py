3fd506f30dce2dd81d5f9e39c13dcbd65f6e03d0
joserapa98 <98589786+joserapa@users.noreply.github.com>
2022-02-23 17:45:36 +0100

    Add einsum

--- tentorch/__init__.py
+++ tentorch/__init__.py
@@ -3,6 +3,7 @@ from tentorch.network_components import AbstractEdge, Edge, ParamEdge
 from tentorch.network_components import TensorNetwork
 
 from tentorch.network_components import (connect, disconnect, contract_edges,
-                                         contract, get_shared_edges, contract_between)
+                                         contract, get_shared_edges, contract_between,
+                                         einsum)
 
 from tentorch.initializers import zeros, ones, copy, rand, randn

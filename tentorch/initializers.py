f9165b162a977a6677cbdf4130c832048e498f6e
joserapa98 <98589786+joserapa@users.noreply.github.com>
2022-02-23 16:44:57 +0100

    Add zeros, ones, copy, rand, randn

--- tentorch/initializers.py
+++ tentorch/initializers.py
@@ -1,2 +1,133 @@
-# TODO: implement functions like tn.ones, tn.zeros that use this functions
-#  to create a node and set its tensor
+"""
+Alternative functions to initialize nodes
+"""
+
+from typing import Optional, Text, Sequence
+from tentorch.network_components import Shape
+
+from tentorch.network_components import AbstractNode, Node, ParamNode
+from tentorch.network_components import TensorNetwork
+
+
+def _initializer(init_method,
+                 shape: Optional[Shape] = None,
+                 axes_names: Optional[Sequence[Text]] = None,
+                 name: Optional[Text] = None,
+                 network: Optional[TensorNetwork] = None,
+                 override_node: bool = False,
+                 param_node: bool = False,
+                 param_edges: bool = False,
+                 **kwargs: float) -> AbstractNode:
+    if param_node:
+        return Node(shape=shape,
+                    axes_names=axes_names,
+                    name=name,
+                    network=network,
+                    override_node=override_node,
+                    param_edges=param_edges,
+                    init_method=init_method,
+                    **kwargs)
+    else:
+        return ParamNode(shape=shape,
+                         axes_names=axes_names,
+                         name=name,
+                         network=network,
+                         override_node=override_node,
+                         param_edges=param_edges,
+                         init_method=init_method,
+                         **kwargs)
+
+
+def zeros(shape: Optional[Shape] = None,
+          axes_names: Optional[Sequence[Text]] = None,
+          name: Optional[Text] = None,
+          network: Optional[TensorNetwork] = None,
+          override_node: bool = False,
+          param_node: bool = False,
+          param_edges: bool = False,
+          **kwargs: float) -> AbstractNode:
+    return _initializer('zeros',
+                        shape=shape,
+                        axes_names=axes_names,
+                        name=name,
+                        network=network,
+                        override_node=override_node,
+                        param_node=param_node,
+                        param_edges=param_edges,
+                        **kwargs)
+
+
+def ones(shape: Optional[Shape] = None,
+         axes_names: Optional[Sequence[Text]] = None,
+         name: Optional[Text] = None,
+         network: Optional[TensorNetwork] = None,
+         override_node: bool = False,
+         param_node: bool = False,
+         param_edges: bool = False,
+         **kwargs: float) -> AbstractNode:
+    return _initializer('ones',
+                        shape=shape,
+                        axes_names=axes_names,
+                        name=name,
+                        network=network,
+                        override_node=override_node,
+                        param_node=param_node,
+                        param_edges=param_edges,
+                        **kwargs)
+
+
+def copy(shape: Optional[Shape] = None,
+         axes_names: Optional[Sequence[Text]] = None,
+         name: Optional[Text] = None,
+         network: Optional[TensorNetwork] = None,
+         override_node: bool = False,
+         param_node: bool = False,
+         param_edges: bool = False,
+         **kwargs: float) -> AbstractNode:
+    return _initializer('copy',
+                        shape=shape,
+                        axes_names=axes_names,
+                        name=name,
+                        network=network,
+                        override_node=override_node,
+                        param_node=param_node,
+                        param_edges=param_edges,
+                        **kwargs)
+
+
+def rand(shape: Optional[Shape] = None,
+         axes_names: Optional[Sequence[Text]] = None,
+         name: Optional[Text] = None,
+         network: Optional[TensorNetwork] = None,
+         override_node: bool = False,
+         param_node: bool = False,
+         param_edges: bool = False,
+         **kwargs: float) -> AbstractNode:
+    return _initializer('rand',
+                        shape=shape,
+                        axes_names=axes_names,
+                        name=name,
+                        network=network,
+                        override_node=override_node,
+                        param_node=param_node,
+                        param_edges=param_edges,
+                        **kwargs)
+
+
+def randn(shape: Optional[Shape] = None,
+          axes_names: Optional[Sequence[Text]] = None,
+          name: Optional[Text] = None,
+          network: Optional[TensorNetwork] = None,
+          override_node: bool = False,
+          param_node: bool = False,
+          param_edges: bool = False,
+          **kwargs: float) -> AbstractNode:
+    return _initializer('randn',
+                        shape=shape,
+                        axes_names=axes_names,
+                        name=name,
+                        network=network,
+                        override_node=override_node,
+                        param_node=param_node,
+                        param_edges=param_edges,
+                        **kwargs)

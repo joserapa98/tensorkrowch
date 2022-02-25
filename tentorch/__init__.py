7ee09f811503c8b65813f9f0965a079675074dc7
joserapa98 <98589786+joserapa@users.noreply.github.com>
2022-02-25 09:45:51 +0100

    Adding stack, unbind

--- tentorch/__init__.py
+++ tentorch/__init__.py
@@ -5,6 +5,6 @@ from tentorch.network_components import TensorNetwork
 from tentorch.network_components import (connect, disconnect, contract_edges,
                                          contract, get_shared_edges, contract_between)
 
-from tentorch.node_operations import einsum, batched_contract_between
+from tentorch.node_operations import einsum, batched_contract_between, stack, unbind
 
 from tentorch.initializers import zeros, ones, copy, rand, randn

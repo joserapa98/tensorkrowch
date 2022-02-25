d8143b0348b0bfcd408441503866dc1a6d736cc4
joserapa98 <98589786+joserapa@users.noreply.github.com>
2022-02-25 13:31:13 +0100

    Correcting stacks

--- tentorch/node_operations.py
+++ tentorch/node_operations.py
@@ -9,7 +9,9 @@ This script contains:
 # split, svd, qr, rq, etc.
 # contract, contract_between, batched_contract_between, einsum, etc.
 
-from typing import (Union, Optional, Sequence, Text, List)
+from typing import (Union, Optional, Sequence, Text, List, Dict)
+
+from tentorch import AbstractEdge
 from tentorch.network_components import Ax, Shape
 from abc import ABC, abstractmethod
 import warnings
@@ -27,7 +29,6 @@ from tentorch.network_components import (connect, disconnect, get_shared_edges,
                                          contract_between)
 
 
-# TODO: comprobar si los edges que se contraen eran de nodos realmente conectados
 def einsum(string: Text, *nodes: AbstractNode) -> Node:
     if '->' not in string:
         raise ValueError('Einsum `string` should have an arrow `->` separating '
@@ -43,18 +44,29 @@ def einsum(string: Text, *nodes: AbstractNode) -> Node:
     matrices = []
     matrices_strings = []
     output_dict = dict(zip(output_string, [0] * len(output_string)))
+    output_char_index = dict(zip(output_string, range(len(output_string))))
     contracted_edges = dict()
-    axes_names = []
-    edges = []
-    node1_list = []
-    for j, input_string in enumerate(input_strings):
-        for k, char in enumerate(input_string):
+    axes_names = dict(zip(range(len(output_string)),
+                          [None] * len(output_string)))
+    edges = dict(zip(range(len(output_string)),
+                     [None] * len(output_string)))
+    node1_list = dict(zip(range(len(output_string)),
+                          [None] * len(output_string)))
+    for i, input_string in enumerate(input_strings):
+        for j, char in enumerate(input_string):
             if char not in output_dict:
+                edge = nodes[i][j]
                 if char not in contracted_edges:
-                    contracted_edges[char] = 1
+                    contracted_edges[char] = [edge]
                 else:
-                    contracted_edges[char] += 1
-                edge = nodes[j][k]
+                    if len(contracted_edges[char]) >= 2:
+                        raise ValueError(f'Subscript {char} appearing more than once in the '
+                                         f'input should be a batch index, but it does not '
+                                         f'appear among the output subscripts')
+                    if edge != contracted_edges[char][0]:
+                        raise ValueError(f'Subscript {char} appears in two nodes that do not '
+                                         f'share a connected edge at the specified axis')
+                    contracted_edges[char] += [edge]
                 if isinstance(edge, ParamEdge):
                     in_matrices = False
                     for mat in matrices:
@@ -66,9 +78,10 @@ def einsum(string: Text, *nodes: AbstractNode) -> Node:
                         matrices.append(edge.matrix)
             else:
                 if output_dict[char] == 0:
-                    axes_names.append(nodes[j].axes[k].name)
-                    edges.append(nodes[j][k])
-                    node1_list.append(nodes[j].axes[k].node1)
+                    k = output_char_index[char]
+                    axes_names[k] = nodes[i].axes[j].name
+                    edges[k] = nodes[i][j]
+                    node1_list[k] = nodes[i].axes[j].node1
                 output_dict[char] += 1
 
     for char in output_dict:
@@ -76,26 +89,22 @@ def einsum(string: Text, *nodes: AbstractNode) -> Node:
             raise ValueError(f'Output subscript {char} must appear among '
                              f'the input subscripts')
     for char in contracted_edges:
-        if contracted_edges[char] == 1:
+        if len(contracted_edges[char]) == 1:
             raise ValueError(f'Subscript {char} appears only once in the input '
                              f'but it not among the output subscripts')
-        elif contracted_edges[char] > 2:
-            raise ValueError(f'Subscript {char} appearing more than once in the '
-                             f'input should be a batch index, but it does not '
-                             f'appear among the output subscripts')
 
     input_string = ','.join(input_strings + matrices_strings)
     einsum_string = input_string + '->' + output_string
     tensors = list(map(lambda n: n.tensor, nodes))
     new_tensor = opt_einsum.contract(einsum_string, *(tensors + matrices))
 
-    new_node = Node(axes_names=axes_names,
+    new_node = Node(axes_names=list(axes_names.values()),
                     name='einsum_node',
                     network=nodes[0].network,
                     param_edges=False,
                     tensor=new_tensor,
-                    edges=edges,
-                    node1_list=node1_list)
+                    edges=list(edges.values()),
+                    node1_list=list(node1_list.values()))
     return new_node
 
 
@@ -203,16 +212,19 @@ def batched_contract_between(node1: AbstractNode,
     return new_node
 
 
-# stack, unbind, stacknode, stackedge, stacked_contraction
-# TODO: implement this, ignore this at the moment
 class StackNode(Node):
-    def __init__(self, nodes: List[AbstractNode]) -> None:
+
+    def __init__(self,
+                 nodes: List[AbstractNode],
+                 name: Optional[Text] = None) -> None:
         same_type = True
         same_shape = True
         same_edge_type = True
+        same_axes_names = True
         for i in range(len(nodes[:-1])):
             same_type &= isinstance(nodes[i], type(nodes[i + 1]))
             same_shape &= nodes[i].shape == nodes[i + 1].shape
+            same_axes_names &= nodes[i].axes_names == nodes[i + 1].axes_names
             for edge1, edge2 in zip(nodes[i].edges, nodes[i + 1].edges):
                 same_edge_type &= isinstance(edge1, type(edge2))
         if not same_type:
@@ -220,6 +232,8 @@ class StackNode(Node):
                             'must be either all Node or all ParamNode type')
         if not same_shape:
             raise ValueError('Cannot stack nodes with different shapes')
+        if not same_axes_names:
+            raise ValueError('Stacked nodes must have the sae name for each axis')
         if not same_edge_type:
             raise TypeError('Cannot stack nodes with edges of different types. '
                             'The edges that are attached to the same axis in '
@@ -228,24 +242,29 @@ class StackNode(Node):
         edges_dict = dict()
         for node in nodes:
             for axis in node.axes:
-                if axis in edges_dict:
-                    edges_dict[axis] = [node[axis]]
+                if axis.name not in edges_dict:
+                    edges_dict[axis.name] = [node[axis]]
                 else:
-                    edges_dict[axis] += [node[axis]]
+                    edges_dict[axis.name] += [node[axis]]
         self._edges_dict = edges_dict
 
         stacked_tensor = torch.stack([node.tensor for node in nodes])
         super().__init__(axes_names=['stack'] + nodes[0].axes_names,
+                         name=name,
                          network=nodes[0].network,
                          tensor=stacked_tensor)
 
-    def make_edge(self, axis: Axis):
+    @property
+    def edges_dict(self) -> Dict[Text, List[AbstractEdge]]:
+        return self._edges_dict
+
+    def make_edge(self, axis: Axis) -> Union['Edge', 'ParamEdge']:
         if axis.num == 0:
             return Edge(node1=self, axis1=axis)
-        if isinstance(self._edges_dict[axis][0], Edge):
-            return StackEdge(self._edges_dict[axis], node1=self, axis1=axis)
-        elif isinstance(self._edges_dict[axis][0], ParamEdge):
-            return ParamStackEdge(self._edges_dict[axis], node1=self, axis1=axis)
+        if isinstance(self.edges_dict[axis.name][0], Edge):
+            return StackEdge(self.edges_dict[axis.name], node1=self, axis1=axis)
+        elif isinstance(self.edges_dict[axis.name][0], ParamEdge):
+            return ParamStackEdge(self.edges_dict[axis.name], node1=self, axis1=axis)
 
 
 class StackEdge(Edge):
@@ -259,6 +278,13 @@ class StackEdge(Edge):
         super().__init__(node1=node1, axis1=axis1,
                          node2=node2, axis2=axis2)
 
+    @property
+    def edges(self) -> List[Edge]:
+        return self._edges
+
+    def __xor__(self, other: 'StackEdge') -> Edge:
+        return connect_stack(self, other)
+
 
 class ParamStackEdge(ParamEdge):
     def __init__(self,
@@ -270,9 +296,28 @@ class ParamStackEdge(ParamEdge):
                          shift=self._edges[0].shift,
                          slope=self._edges[0].slope)
 
+    @property
+    def edges(self) -> List[ParamEdge]:
+        return self._edges
 
-def stack(nodes: List[AbstractNode]):
-    return StackNode(nodes)
+    def __xor__(self, other: 'ParamStackEdge') -> ParamEdge:
+        return connect_stack(self, other)
+
+
+def connect_stack(edge1: Union[StackEdge, ParamStackEdge],
+                  edge2: Union[StackEdge, ParamStackEdge],
+                  override_network: bool = False):
+    if edge1.edges != edge2.edges:
+        raise ValueError('Cannot connect stack edges whose lists of'
+                         ' edges are not the same. They will be the '
+                         'same when both lists contain edges connecting'
+                         ' the nodes that formed the stack nodes.')
+    return connect(edge1=edge1, edge2=edge2,
+                   override_network=override_network)
+
+
+def stack(nodes: List[AbstractNode], name: Optional[Text] = None):
+    return StackNode(nodes, name=name)
 
 
 def unbind():

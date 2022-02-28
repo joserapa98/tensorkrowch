3f57e2564d12a469487e7dbd61835c280cf8ee43
joserapa98 <98589786+joserapa@users.noreply.github.com>
2022-02-28 16:08:24 +0100

    Change stacked_einsum

--- tentorch/__init__.py
+++ tentorch/__init__.py
@@ -5,6 +5,6 @@ from tentorch.network_components import TensorNetwork
 from tentorch.network_components import (connect, disconnect, contract_edges,
                                          contract, get_shared_edges, contract_between)
 
-from tentorch.node_operations import einsum, batched_contract_between, stack, unbind, stacked_contract
+from tentorch.node_operations import einsum, batched_contract_between, stack, unbind, stacked_einsum
 
 from tentorch.initializers import zeros, ones, copy, rand, randn

7cc00a65a4a4c2978ec40bdead6c9ae1cebe1bff
joserapa98 <98589786+joserapa@users.noreply.github.com>
2022-03-02 11:19:54 +0100

    Add get_batch_edges

--- tentorch/__init__.py
+++ tentorch/__init__.py
@@ -2,9 +2,11 @@ from tentorch.network_components import AbstractNode, Node, ParamNode
 from tentorch.network_components import AbstractEdge, Edge, ParamEdge
 from tentorch.network_components import TensorNetwork
 
-from tentorch.network_components import (connect, disconnect, contract_edges,
-                                         contract, get_shared_edges, contract_between)
+from tentorch.network_components import (connect, disconnect, get_shared_edges,
+                                         get_batch_edges, contract_edges, contract,
+                                         contract_between)
 
-from tentorch.node_operations import einsum, batched_contract_between, stack, unbind, stacked_einsum
+from tentorch.node_operations import (einsum, batched_contract_between,
+                                      stack, unbind, stacked_einsum)
 
 from tentorch.initializers import zeros, ones, copy, rand, randn

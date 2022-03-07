35e14a02a4eee9b7e22758f20298314bbb58f2ac
joserapa98 <98589786+joserapa@users.noreply.github.com>
2022-03-07 13:49:38 +0100

    Add unit embedding

---
+++ tentorch/tn_models/embeddings.py
@@ -0,0 +1,24 @@
+"""
+Embedding methods
+"""
+
+from math import pi, sqrt
+import torch
+
+from tentorch.utils import comb_num
+
+
+def unit(data: torch.Tensor, dim: int) -> torch.Tensor:
+    """
+    Parameters
+    ----------
+    data: input tensor with shape batch x 1 -> batch x dim
+    dim: embedding dimension
+    """
+    embedded_data = torch.empty(data.shape[0], dim)
+    for i in range(1, dim + 1):
+        aux = sqrt(comb_num(dim - 1, i - 1)) *\
+              (pi / 2 * data).cos().pow(dim - i) *\
+              (pi / 2 * data).sin().pow(i - 1)
+        embedded_data[:, i - 1] = aux
+    return embedded_data

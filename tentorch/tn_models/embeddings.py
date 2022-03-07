7672e22a5aa1a2af133e94d62e6d0d756d1c30a9
joserapa98 <98589786+joserapa@users.noreply.github.com>
2022-03-07 16:41:58 +0100

    Correct unit

--- tentorch/tn_models/embeddings.py
+++ tentorch/tn_models/embeddings.py
@@ -18,7 +18,7 @@ def unit(data: torch.Tensor, dim: int) -> torch.Tensor:
     embedded_data = torch.empty(data.shape[0], dim)
     for i in range(1, dim + 1):
         aux = sqrt(comb_num(dim - 1, i - 1)) *\
-              (pi / 2 * data).cos().pow(dim - i) *\
-              (pi / 2 * data).sin().pow(i - 1)
+              (pi / 2 * data[:, 0]).cos().pow(dim - i) *\
+              (pi / 2 * data[:, 0]).sin().pow(i - 1)
         embedded_data[:, i - 1] = aux
     return embedded_data

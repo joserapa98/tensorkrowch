95e36f9683c1b1934dc7840766a08c92aa820419
joserapa98 <joserapa98@gmail.com>
2022-09-28 11:47:13 +0200

    Add TODO

--- tentorch/network_operations.py
+++ tentorch/network_operations.py
@@ -4,3 +4,5 @@
 # TODO: functions to find contraction paths using opt_einsum (like tensornetwork)
 # TODO: implement einsum for Nodes, so that edge links are automatically
 #  made and we can compute contractions between several nodes at the same time -> schematic way of constructing TN
+# TODO: Para MPO, que en la función forward puedas meter solo data, o un MPS y data aparte, para que entienda que
+#  el resultado de MPS * MPO tiene que ser un MPS

02edfd71784fae66ca44c20dbf50de1fb3f25b40
joserapa98 <joserapa98@gmail.com>
2023-05-16 15:55:12 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -2,27 +2,28 @@ from setuptools import setup
 from torch.utils.cpp_extension import CppExtension, BuildExtension
 from pathlib import Path
 
-LIB_NAME = "tensorkrowch"
-version = "0.1.0"
+LIB_NAME = 'tensorkrowch'
+version = '1.0.0'
 
 root_dir = Path(LIB_NAME)
-include_dir = root_dir / "include"
-ext_src = [str(x.absolute()) for x in root_dir.glob("*.cpp")]
+include_dir = root_dir / 'include'
+ext_src = [str(x.absolute()) for x in root_dir.glob('csrc/*.cpp')]
 
 cpp_extension = CppExtension(
-    LIB_NAME + "._C",
+    LIB_NAME + '._C',
     sources=ext_src,
 )
 
 setup(
     name=LIB_NAME,
     version=version,
-    description="A 100x faster PyTorch SVD",
-    author="Chengqi Deng",
-    license="MIT",
-    python_requires=">=3.6",
-    install_requires=["torch>=1.0"],
-    packages=[LIB_NAME, "tensorkrowch.tn_models"],
+    description='Tensor Networks with PyTorch',
+    author='José Ramón Pareja Monturiol, David Pérez-García, Alejandro Pozas-Kerstjens',
+    author_email='joserapa@gmail.com',
+    license='MIT',
+    python_requires='>=3.6',
+    install_requires=['torch>=1.0'],
+    packages=[LIB_NAME, 'tensorkrowch.models'],
     ext_modules=[cpp_extension],
-    cmdclass={"build_ext": BuildExtension},
+    cmdclass={'build_ext': BuildExtension},
 )

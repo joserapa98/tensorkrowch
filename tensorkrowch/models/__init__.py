bc06ebcf6a3f0dfda7e79811eed3ac4fa05ba06b
joserapa98 <joserapa98@gmail.com>
2023-05-22 23:41:21 +0200

    Fix indent

--- tensorkrowch/models/__init__.py
+++ tensorkrowch/models/__init__.py
@@ -1,5 +1,5 @@
 from tensorkrowch.models.mps import MPS, UMPS, ConvMPS, ConvUMPS
 from tensorkrowch.models.mps_layer import (MPSLayer, UMPSLayer,
-                                              ConvMPSLayer, ConvUMPSLayer)
+                                           ConvMPSLayer, ConvUMPSLayer)
 from tensorkrowch.models.peps import PEPS, UPEPS, ConvPEPS, ConvUPEPS
 from tensorkrowch.models.tree import Tree, UTree, ConvTree, ConvUTree

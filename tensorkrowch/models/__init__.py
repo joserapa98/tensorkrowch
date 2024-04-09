d2354c1a01f341e602f44f7fd7b54f00b3dccd23
joserapa98 <joserapa98@gmail.com>
2024-04-09 16:50:32 +0200

    Reorganize mps and mpo classes

--- tensorkrowch/models/__init__.py
+++ tensorkrowch/models/__init__.py
@@ -1,5 +1,5 @@
-from tensorkrowch.models.mps import MPS, UMPS, ConvMPS, ConvUMPS
-from tensorkrowch.models.mps_layer import (MPSLayer, UMPSLayer,
-                                           ConvMPSLayer, ConvUMPSLayer)
+from tensorkrowch.models.mps import (MPS, UMPS, MPSLayer, UMPSLayer, MPSData,
+                                ConvMPS, ConvUMPS, ConvMPSLayer, ConvUMPSLayer)
+from tensorkrowch.models.mpo import MPO, UMPO
 from tensorkrowch.models.peps import PEPS, UPEPS, ConvPEPS, ConvUPEPS
 from tensorkrowch.models.tree import Tree, UTree, ConvTree, ConvUTree

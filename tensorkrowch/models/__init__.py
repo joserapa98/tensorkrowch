d2d605331e271d097b9162cd723a0dda4cda92c2
joserapa98 <joserapa98@gmail.com>
2024-04-12 14:14:43 +0200

    Move MPSData to other file

--- tensorkrowch/models/__init__.py
+++ tensorkrowch/models/__init__.py
@@ -1,5 +1,6 @@
-from tensorkrowch.models.mps import (MPS, UMPS, MPSLayer, UMPSLayer, MPSData,
-                                ConvMPS, ConvUMPS, ConvMPSLayer, ConvUMPSLayer)
+from tensorkrowch.models.mps_data import MPSData
 from tensorkrowch.models.mpo import MPO, UMPO
+from tensorkrowch.models.mps import (MPS, UMPS, MPSLayer, UMPSLayer,
+                                ConvMPS, ConvUMPS, ConvMPSLayer, ConvUMPSLayer)
 from tensorkrowch.models.peps import PEPS, UPEPS, ConvPEPS, ConvUPEPS
 from tensorkrowch.models.tree import Tree, UTree, ConvTree, ConvUTree
